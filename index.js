88f20b6ec1a71f023778c1f0e16828400b0f42ab
praveen-ux <116873039+praveen-ux@users.noreply.github.com>
2023-07-01 02:01:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('HI PRAVEEN'); //write a response to the client
+  res.write('HI hello PRAVEEN'); //write a response to the client
   res.end(); //end the response
 }).listen(3000); //the server object listens on port 3000
